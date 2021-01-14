469397e2eb25e967edded0973163ba8ca1d1b260
misaka4e21 <misaka4e21@gmail.com>
2021-01-14 23:21:10 +0800

    Use callbacks.

--- migrator/main.go
+++ migrator/main.go
@@ -3,19 +3,42 @@ package migrator
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/callbacks"
 )
+
+func autoMigrate(db *gorm.DB, models ...interface{}) (strs []string, err error) {
+	db.Callback().Raw().Remove("gorm:raw")
+	db.Callback().Raw().Register("migrations", func(db *gorm.DB) {
+		stmt := db.Statement
+		sql := stmt.SQL.String()
+		if strings.HasPrefix(sql, "CREATE") || strings.HasPrefix(sql, "ALTER") || strings.HasPrefix(sql, "DROP") {
+			strs = append(strs, sql)
+		} else if db.Error == nil && !db.DryRun {
+			result, err := db.Statement.ConnPool.ExecContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
+			if err != nil {
+				db.AddError(err)
+			} else {
+				db.RowsAffected, _ = result.RowsAffected()
+			}
+		}
+	})
+	err = db.AutoMigrate(models...)
+	db.Callback().Raw().Remove("migrations")
+	db.Callback().Raw().Register("gorm:raw", callbacks.RawExec)
+	return
+}
 
 // GenerateMigrations writes migration files.
 func GenerateMigrations(migrationName string, db *gorm.DB, values ...interface{}) error {
-	mdb := NewMigratorDB(*db)
-	err := mdb.AutoMigrate(values...)
+	sqls, err := autoMigrate(db, values...)
 	if err != nil {
 		return err
 	}
-	if len(mdb.sqls) == 0 {
+	if len(sqls) == 0 {
 		fmt.Println("Nothing to generate.")
 		return nil
 	}
@@ -25,7 +48,7 @@ func GenerateMigrations(migrationName string, db *gorm.DB, values ...interface{}
 	if err != nil {
 		return err
 	}
-	_, err = f.Write([]byte(mdb.GetMigrationString()))
+	_, err = f.Write([]byte(strings.Join(sqls, ";\n")))
 	if err != nil {
 		return err
 	}
